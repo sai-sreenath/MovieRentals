853043773de6bcdee7abec3c3d6d39eb3bd699d1
sai-sreenath <sai-sreenath@users.noreply.github.com>
2019-12-16 09:26:03 -0500

    context to get the customers from the DB instead rather than hardcode values

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -9,26 +9,39 @@ namespace VideoRentals.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context=new ApplicationDbContext(); 
+        }
+
+        //DBContext is a Disposable object,Hence overriding
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
-
+            //var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
             return View(customers);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "John Smith"},
-                new Customer {Id = 2, Name = "Mary Williams"}
-            };
-        }
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer {Id = 1, Name = "John Smith"},
+        //        new Customer {Id = 2, Name = "Mary Williams"}
+        //    };
+        //}
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
