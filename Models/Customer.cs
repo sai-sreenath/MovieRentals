aca8f33b9c559ad0ab9f2f12e0c8321daf06533e
sai-sreenath <sai-sreenath@users.noreply.github.com>
2019-12-16 00:52:34 -0500

    Implemented code first workflow,Model changes and updating into database

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,5 +9,8 @@ namespace VideoRentals.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
     }
 }
