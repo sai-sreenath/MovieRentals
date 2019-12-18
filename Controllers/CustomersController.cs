db750eb748ca727a1c6bf7f8970a7d203b004bb7
sai-sreenath <sai-sreenath@users.noreply.github.com>
2019-12-17 21:44:57 -0500

    Saving data into customers DB

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -35,9 +35,12 @@ namespace VideoRentals.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(NewCustomerViewModel viewModel)
+        public ActionResult Create(Customer customer)
         {
-            return View(); 
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
         }
         
         // GET: Customers
