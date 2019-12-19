92358573a2404ea928fcd5a9df9beb3b069a29af
sai-sreenath <sai-sreenath@users.noreply.github.com>
2019-12-18 19:54:41 -0500

    validation summary impl,not displaying customer id as validation error

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -31,6 +31,7 @@ namespace VideoRentals.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
             return View("CustomerForm",viewModel);
