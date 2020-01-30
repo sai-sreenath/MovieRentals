d04014189c2f6344c9fa6c58f2eddabd4d3ee6de
sai-sreenath <sai-sreenath@users.noreply.github.com>
2020-01-30 10:29:52 -0500

    Error displaying genre in movies list get Api

--- Dtos/GenreDto.cs
+++ Dtos/GenreDto.cs
@@ -8,6 +8,6 @@ namespace VideoRentals.Dtos
     public class GenreDto
     {
         public byte Id { get; set; }
-        public string Name { get; set; }
+        public string GenreName { get; set; }
     }
 }
