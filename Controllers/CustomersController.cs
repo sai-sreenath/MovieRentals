ddee3126173e526242e482f603124277d60ea013
sai-sreenath <sai-sreenath@users.noreply.github.com>
2020-02-01 10:58:01 -0500

    Disabling Memory caching

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -76,12 +76,6 @@ namespace VideoRentals.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            if (MemoryCache.Default["Genres"] == null)
-            {
-                MemoryCache.Default["Genres"] = _context.Genres.ToList();
-            }
-
-            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
 
             return View();
         }
